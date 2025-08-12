4f39ab590b0ad609ce98dfa83541b3be8c799bd6
luka2002 <lukav0311@gmail.com>
2025-08-13 00:13:22 +0200

    frontend test version

--- frontend/react-frontend/src/App.jsx
+++ frontend/react-frontend/src/App.jsx
@@ -1,23 +1,48 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './components/pages/Home';
-import Navbar from './components/Navbar';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { useState } from "react";
+import Registration from "./components/Registration";
+import { Login } from "./components/Login";
+import "./App.css";
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Home from "./components/pages/Home";
+import { apiService } from './components/ApiService';
+import Profile from './components/Profile'
+import CreatePlan from './components/CreatePlan';
+import Runners from "./components/Runners";
+import PlanRun from "./components/PlanRun";
+import StatisticRun from "./components/StatisticRun";
+import MapPage from "./components/MapPage";
+import EndRun from './components/EndRun';
+import Comments from './components/Comments';
+import AllStatistics from './components/AllStatistics';
 
 function App() {
-  return (
 
-    <BrowserRouter>
-    
-    <div>
-      <Navbar />
-     
-    </div>
+  const [token, setToken] = useState(apiService.getToken());
+
+  return (
+    <Router className="App">
+      <Navbar apiService={apiService} />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/registracija" element={<Registration />} />
+        <Route path="/login" element={<Login updateToken={(token) => setToken(token)} />} ></Route>
+        <Route path="/logout" element={<Navigate to="/" />}></Route>
+        <Route path="/moj-nalog" element={<Profile />} />
+        <Route path="/kreiraj-plan" element={<CreatePlan />} />
+        <Route path="/trkaci" element={<Runners />} />
+        <Route path="/planovi-trka" element={<PlanRun />} />
+        <Route path="/statistike-trke" element={<StatisticRun />} />
+        <Route path="/map" element={<MapPage />} />
+        <Route path="/zavrsi-trku/:planId" element={<EndRun />} />
+        <Route path="/komentari" element={<Comments />} />
+        <Route path="/sve-statistike" element={<AllStatistics />} />
+
       </Routes>
-      <Footer></Footer>
-    </BrowserRouter>
+      <Footer />
+    </Router>
   );
 }
-
 export default App;
---
+++ frontend/react-frontend/src/components/EndRun.js
@@ -0,0 +1,95 @@
+
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { apiService } from './ApiService';
+import './EndRun.css';
+import Trkac from "../assets/pozadina2.png";
+import { Button } from './Button';
+import Swal from 'sweetalert2';
+
+const EndRun = () => {
+  const { planId } = useParams();
+  const [ukupnoVreme, setUkupnoVreme] = useState('');
+  const [predjeniKm, setPredjeniKm] = useState('');
+  const [loggedInTrkacId, setLoggedInTrkacId] = useState(null);
+
+  useEffect(() => {
+    const loggedInTrkacInfo = apiService.getLoginInfo();
+    setLoggedInTrkacId(loggedInTrkacInfo.id);
+  }, []);
+
+  const handlePotvrdi = async () => {
+    try {
+
+      if (!ukupnoVreme || !predjeniKm) {
+        Swal.fire({
+          icon: 'warning',
+          title: 'Molimo popunite sva polja.',
+        });
+        return;
+      }
+
+      await apiService.createRunningStatistics({
+        ukupno_vreme: ukupnoVreme,
+        predjeni_km: predjeniKm,
+        plan_trke_id: planId,
+        trkac_id: loggedInTrkacId,
+      });
+
+      Swal.fire({
+        icon: 'success',
+        title: 'Trka je uspešno završena!',
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    } catch (error) {
+      console.error('Greška pri završetku trke:', error);
+      Swal.fire({
+        icon: 'error',
+        title: 'Došlo je do greške prilikom završetka trke.',
+      });
+    }
+  };
+
+  return (
+    <div className="outer-container" style={{ backgroundImage: `url(${Trkac})`, backgroundSize: '100%', backgroundPosition: 'right' }}>
+      <div className="container">
+        <div className="content-container">
+          <h1 style={{ marginLeft: '-114px', color: 'white', fontFamily: 'Work Sans sans-serif' }}>Završi trku</h1>
+          <form>
+            <label style={{ color: 'white', fontFamily: 'Work Sans sans-serif' }}>
+              Ukupno vreme:
+              <input
+                type="text"
+                value={ukupnoVreme}
+                onChange={(e) => setUkupnoVreme(e.target.value)}
+              />
+            </label>
+            <br />
+            <label style={{ color: 'white', fontFamily: 'Work Sans sans-serif' }}>
+              Predjeni kilometri:
+              <input
+                type="text"
+                value={predjeniKm}
+                onChange={(e) => setPredjeniKm(e.target.value)}
+              />
+            </label>
+            <br />
+            <Button
+              type="button"
+              onClick={handlePotvrdi}
+              buttonStyle="btn--outline"
+              buttonSize="btn--small"
+              style={{ marginLeft: '150px' }}
+            >
+              Potvrdi
+            </Button>
+          </form>
+        </div>
+      </div>
+      <div className="background-behind-container"></div>
+    </div>
+  );
+}
+
+export default EndRun;
